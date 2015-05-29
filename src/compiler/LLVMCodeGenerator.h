87053c49059edaf741325ce915ceeefd96ca64cc
Peixian Wang <peixian@peixian.me>
2015-05-28 19:12:15 -0500

    LLVM Verifier location was changed
    Changed from /Analysis/Verifier.h to /IR/Verifier.h

--- src/compiler/LLVMCodeGenerator.h
+++ src/compiler/LLVMCodeGenerator.h
@@ -7,7 +7,7 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
-#include <llvm/Analysis/Verifier.h>
+#include <llvm/IR/Verifier.h>
 
 class LLVMCodeGenerator : public ASTNodeVisitor {
 	public:
